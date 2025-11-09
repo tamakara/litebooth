5f488d788050ea85d86d65280034416e9f13a552
tamakara <tamakara@qq.com>
2025-11-09 15:15:55 +0800

    初始化项目目录

---
+++ backend/src/main/java/com/tamakara/litebooth/LiteboothApplication.java
@@ -0,0 +1,13 @@
+package com.tamakara.litebooth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteboothApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteboothApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/tamakara/litebooth/LiteboothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamakara.litebooth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteboothApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
